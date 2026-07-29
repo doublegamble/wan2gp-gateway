6910ba94726cffc8de20af3f1aa82eeca96da214
doublegamble <doublegamble666@gmail.com>
2026-07-29 10:48:19 +0800

    fix: use EXTERNAL_PORT for discovery broadcast so mapped ports are correctly registered

--- discovery.py
+++ discovery.py
@@ -43,7 +43,7 @@ def set_active_ip(new_ip):
 def get_node_info():
     local_ip = get_local_ip()
     hostname = os.getenv("HOST_NAME") or os.getenv("COMPUTERNAME") or socket.gethostname()
-    api_port = os.getenv("GATEWAY_PORT", "50080")
+    api_port = os.getenv("EXTERNAL_PORT") or os.getenv("GATEWAY_PORT", "50080")
     base_url = os.getenv("WAN2GP_BASE_URL", f"http://{local_ip}:{api_port}")
     ip_slug = local_ip.replace('.', '_')
     
